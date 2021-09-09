de996d97eb1c0a6f03d7ca077395cffb1f48cd5e
Michael Feeney <michael.feeney@badmonster.com>
2021-09-09 12:54:21 -0400

    Day 2

---
+++ INFO6044/BasicClasses/BasicClasses/HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>   // "console" out
+#include <stdio.h>	// Using the "C" libraries
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "HEY!" << std::endl;
+
+	// Using the "C" standard IO
+	printf("Hello world!");
+
+	return 0;
+}
+
